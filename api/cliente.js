07c9cd03c2685b898918d9db8f7152e1f241602e
CleytonPinheiro <cleyton_pin@hotmail.com>
2020-07-08 12:03:38 -0300

    Trabalhando com mensagens ao usuário, conforme ações do mesmo no form.

--- api/cliente.js
+++ api/cliente.js
@@ -1,62 +1,60 @@
 const listarClientes = () => {
-    return fetch("http:localhost:4000/clientes", {
-        method: "GET"
+  return fetch("http:localhost:4000/clientes", {
+    method: "get"
+  })
+    .then(resp => {
+      return resp.json();
     })
-        .then(resp => {
-            return resp.json();
-        })
+    .then(json => {
+      return json;
+    });
 };
 
 const cadastrarClientes = (nome, cpf) => {
-    const json = JSON.stringify({
-        nome: nome,
-        cpf: cpf
-    });
+  const json = JSON.stringify({
+    nome: nome,
+    cpf: cpf
+  });
 
-    return fetch("http:localhost:4000/clientes/cliente", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: json
-    }).then(resp => {
+  return fetch("http:localhost:4000/clientes/cliente", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: json
+  }).then(resp => {
 
-            return resp.body;
-        }
-    );
+      return resp.body;
+    }
+  );
 }
 
-    const deletaCliente = id => {
-        return fetch(`http:localhost:4000/clientes/cliente/${id}`, {
-            method: "DELETE",
+const deletaCliente = id => {
+  return fetch(`http:localhost:4000/clientes/cliente/${id}`, {
+    method: "DELETE",
+})}
+
+const detalhaCliente = id => {
+    return fetch(`http://localhost:4000/clientes/cliente/${id}`,{
+        method: 'GET'
+    })
+        .then(resposta => {
+            return resposta.json()
         })
 }
 
-    const detalhaCliente = id => {
-        return fetch(`http://localhost:4000/clientes/cliente/${id}`,
-            {
-                method:"GET"
-            })
-            .then(resp =>{
-                return resp.json()
-            })
-}
-
-const editaCliente = (id, cpf, nome) =>{
+const editaCliente = (id, cpf, nome) => {
     const json = JSON.stringify({
         nome: nome,
         cpf: cpf
-    });
-
-    return fetch(`http:localhost:4000/clientes/cliente/${id}`, {
-        method: "PUT",
+    })
+    return fetch(`http://localhost:4000/clientes/cliente/${id}`,{
+        method: 'PUT',
         headers: {
-            "Content-Type": "application/json"
+            'Content-type': 'application/json'
         },
         body: json
-    }).then(resp => {
-
-            return resp.body;
-        }
-    );
+    })
 }
+
+
--- edita-cliente.js
+++ edita-cliente.js
@@ -1,24 +1,54 @@
-const pegaUrl = new URL(window.location)
+const pegaURL = new URL(window.location)
 
-const id = pegaUrl.searchParams.get('id')
+const id = pegaURL.searchParams.get('id')
 
-const inputCpf = document.querySelector('[data-cpf]')
+const inputCPF = document.querySelector('[data-cpf]')
 const inputNome = document.querySelector('[data-nome]')
 
 detalhaCliente(id).then( dados => {
-    inputCpf.value = dados[0].cpf
+    inputCPF.value = dados[0].cpf
     inputNome.value = dados[0].nome
 })
 
 const formEdicao = document.querySelector('[data-form]')
 
+const alerta = (classe, mensagem) => {
+    const linha = document.createElement('tr');
+
+    const conteudoLinha = `
+    <div class="${classe}">${mensagem}</div>
+
+`
+
+    linha.innerHTML = conteudoLinha;
+    return linha;
+}
 formEdicao.addEventListener('submit', event => {
     event.preventDefault()
 
-    if(validaCPF(inputCpf.value)){
-        alert("CPF inexistente")
+    if(!validaCPF(inputCPF.value)){
+        alert("ESSE NÃO EXISTE")
         return
     }
 
-    editaCliente(id, inputCpf.value, inputNome.value )
+    if(inputCPF.value.length !== 11){
+        alert("ESSE CPF NÃO EXISTE")
+        return
+    }
+
+    editaCliente(id, inputCPF.value, inputNome.value)
+    .then( resposta => {
+        if( resposta.status === 200){
+            formEdicao.appendChild(alerta(
+                "alert alert-success",
+                "CLIENTE EDITADO COM SUCESSO !"
+            ))
+        } else {
+            formEdicao.appendChild(alerta(
+                "alert alert-warning",
+                "O CLIENTE NÃO PODE SER EDITADO !"
+            ))
+        }
+    })
+
 })
